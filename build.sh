0dbae83811e5ad1daeb9ca677824ff256abcff12
sparky4 <sparky4@cock.li>
2025-09-24 03:08:27 -0500

    stacksize needs to be worked on

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wcl -0 -lr -ml -os -wx -we -d0 -k20480 -i=src/ -i=src/zlib/ -DNOREPOS -DNOLZMA -fe=fdnpkg16.exe src/*.c src/zlib/zlib_l.lib
+wcl -0 -lr -ml -os -wx -we -d0 -k40960 -i=src/ -i=src/zlib/ -DNOREPOS -DNOLZMA -fe=fdnpkg16.exe src/*.c src/zlib/zlib_l.lib
 upx --8086 -9 fdnpkg16.exe
