fe8c82c343f148a84017bb0e87a94139a7c5c1ad
sparky4 <sparky4@cock.li>
2025-09-24 17:34:15 -0500

    zlib make added and added some utility scripts

--- build.bat
+++ build.bat
@@ -1,12 +1,7 @@
 @echo off
-
+wcl -0 -lr -ml -ox -sg -wx -we -d0 -k40960 -i=src/ -i=src/zlib/ -DNOREPOS -DNOLZMA -fe=fdnpkg16.exe src/*.c src/zlib/zlib_l.lib
 rem wcl -0 -lr -ml -os -wx -we -d0 -i=..\ -i=..\zlib\ -DNOREPOS -DNOLZMA -fe=fdnpkg16.exe src\*.c ..\zlib\zlib_l.lib
-wcl -0 -lr -ml -os -wx -we -d0 -k40960 -i=src/ -i=src/zlib/ -DNOREPOS -DNOLZMA -fe=fdnpkg16.exe src/*.c src/zlib/zlib_l.lib
+rem wcl -0 -lr -ml -os -wx -we -d0 -k40960 -i=src/ -i=src/zlib/ -DNOREPOS -DNOLZMA -fe=fdnpkg16.exe src/*.c src/zlib/zlib_l.lib
 if ERRORLEVEL 1 goto gameover
-
-rem del src\*.c
-rem rmdir src
-
 upx --8086 -9 fdinst.exe
-
 :gameover
---
+++ pkgunzip.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# this script just updates the freedos fdnpkg16 zip package's contents
+echo "unzip"
+cp -rp fdnpkg16.zip ../
+mkdir fdnpkg16
+mkdir .temp
+mv fdnpkg16.zip fdnpkg16/
+mv *.zip .temp/
+mv *.7z .temp/
+cd fdnpkg16
+unzip fdnpkg16.zip
+rm fdnpkg16.zip
+cd ..
