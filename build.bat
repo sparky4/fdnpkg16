1a01d92106912d35e3a7e0326ac08d108526e37d
sparky4 <sparky4@cock.li>
2025-09-25 15:42:21 -0500

    32k should be stack size now because the program crashed on 1 pc

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-wcl -0 -lr -ml -opnr -oe=24 -oil+ -outback -ohm -sg -wx -we -d0 -k20480 -i=src -i=src\zlib -fe=fdnpkg16.exe src\*.c src\zlib\zlib_l.lib
+wcl -0 -lr -ml -opnr -oe=24 -oil+ -outback -ohm -sg -wx -we -d0 -k32768 -i=src -i=src\zlib -fe=fdnpkg16.exe src\*.c src\zlib\zlib_l.lib
 rem wcl -0 -lr -ml -os -wx -we -d0 -i=..\ -i=..\zlib\ -DNOREPOS -DNOLZMA -fe=fdnpkg16.exe src\*.c ..\zlib\zlib_l.lib
 rem wcl -0 -lr -ml -os -wx -we -d0 -k40960 -i=src\ -i=src\zlib\ -DNOREPOS -DNOLZMA -fe=fdnpkg16.exe src\*.c src\zlib\zlib_l.lib
 if ERRORLEVEL 1 goto gameover
