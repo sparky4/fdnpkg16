c8476c6d14ac031def8a4e3c71006618a2c31f2e
sparky4 <sparky4@cock.li>
2026-02-01 11:30:06 -0600

    another config file for latest test distro

--- pkgzip.sh
+++ pkgzip.sh
@@ -22,6 +22,7 @@ cp -rp fdpget.bat fdnpkg16/BIN/
 cp -rp fdnpkg16.cfg fdnpkg16/BIN/
 cp -rp fdnpkg16.oem fdnpkg16/BIN/
 cp -rp fdnpkg16.4 fdnpkg16/BIN/
+cp -rp fdnpkg16.lat fdnpkg16/BIN/
 cp -rp fdnpkg16.ico fdnpkg16/BIN/
 #. ./build.sh
 wmake
