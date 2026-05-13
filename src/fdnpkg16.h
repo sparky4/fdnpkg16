67a617cc8488fcfa5be7068e84d7bd282c52798e
sparky4 <sparky4@cock.li>
2026-05-13 10:35:06 -0500

    added back no updating if there is an error on cache database creation

--- src/fdnpkg16.h
+++ src/fdnpkg16.h
@@ -19,7 +19,7 @@
 //#define DEBUG                 // uncomment this to enable debug mode
 //#define USE_INTERNAL_WATTCP   // enable/disable this to use external or internal watt32
 //#define USE_MTCP              // enable/disable this to use mtcp or watt32 externally
-#define ERRCACHE              // enable this to have databases, always. no matter what fails
+//#define ERRCACHE              // enable this to have databases, always. no matter what fails
 
 struct flist_t {
   struct flist_t *next;
