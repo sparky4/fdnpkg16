c9f52ff92855830a7fb29e17dc1a5fb234030dfd
Katheryn <katheryn@mary630>
2026-05-27 21:08:14 -0400

    scripts updated bibabo

--- cptoutf8.sh
+++ cptoutf8.sh
@@ -2,5 +2,31 @@
 cd fdnpkg16/NLS
 
 iconv -f cp437 -t utf8 fdnpkg16.en > fdnpkg16.en8
+iconv -f cp865 -t utf8 fdnpkg16.fi > fdnpkg16.fi8
+iconv -f cp850 -t utf8 fdnpkg16.es > fdnpkg16.es8
+iconv -f cp852 -t utf8 fdnpkg16.cs > fdnpkg16.cs8
+iconv -f cp850 -t utf8 fdnpkg16.da > fdnpkg16.da8
+iconv -f cp850 -t utf8 fdnpkg16.de > fdnpkg16.de8
+iconv -f cp858 -t utf8 fdnpkg16.eo > fdnpkg16.eo8
+iconv -f cp775 -t utf8 fdnpkg16.et > fdnpkg16.et8
+iconv -f cp850 -t utf8 fdnpkg16.fr > fdnpkg16.fr8
+iconv -f cp852 -t utf8 fdnpkg16.hu > fdnpkg16.hu8
+iconv -f cp861 -t utf8 fdnpkg16.is > fdnpkg16.is8
+iconv -f cp858 -t utf8 fdnpkg16.it > fdnpkg16.it8
+iconv -f cp775 -t utf8 fdnpkg16.lt > fdnpkg16.lt8
+iconv -f cp775 -t utf8 fdnpkg16.lv > fdnpkg16.lv8
+iconv -f cp858 -t utf8 fdnpkg16.nl > fdnpkg16.nl8
+iconv -f cp865 -t utf8 fdnpkg16.no > fdnpkg16.no8
+iconv -f cp860 -t utf8 fdnpkg16.pb > fdnpkg16.pb8
+iconv -f cp852 -t utf8 fdnpkg16.pl > fdnpkg16.pl8
+iconv -f cp860 -t utf8 fdnpkg16.pt > fdnpkg16.pt8
+iconv -f cp866 -t utf8 fdnpkg16.ru > fdnpkg16.ru8
+iconv -f cp852 -t utf8 fdnpkg16.si > fdnpkg16.si8
+iconv -f cp852 -t utf8 fdnpkg16.sk > fdnpkg16.sk8
+iconv -f cp852 -t utf8 fdnpkg16.sl > fdnpkg16.sl8
+iconv -f cp855 -t utf8 fdnpkg16.sr > fdnpkg16.sr8
+iconv -f cp858 -t utf8 fdnpkg16.sv > fdnpkg16.sv8
+iconv -f cp857 -t utf8 fdnpkg16.tr > fdnpkg16.tr8
+iconv -f cp855 -t utf8 fdnpkg16.uk > fdnpkg16.uk8
 
 cd ../..
--- utf8tocp.sh
+++ utf8tocp.sh
@@ -1,6 +1,32 @@
 cd fdnpkg16/NLS
 
 iconv -t cp437 -f utf8 fdnpkg16.en8 > fdnpkg16.en
+iconv -t cp865 -f utf8 fdnpkg16.fi8 > fdnpkg16.fi
+iconv -t cp850 -f utf8 fdnpkg16.es8 > fdnpkg16.es
+iconv -t cp852 -f utf8 fdnpkg16.cs8 > fdnpkg16.cs
+iconv -t cp850 -f utf8 fdnpkg16.da8 > fdnpkg16.da
+iconv -t cp850 -f utf8 fdnpkg16.de8 > fdnpkg16.de
+iconv -t cp858 -f utf8 fdnpkg16.eo8 > fdnpkg16.eo
+iconv -t cp775 -f utf8 fdnpkg16.et8 > fdnpkg16.et
+iconv -t cp850 -f utf8 fdnpkg16.fr8 > fdnpkg16.fr
+iconv -t cp852 -f utf8 fdnpkg16.hu8 > fdnpkg16.hu
+iconv -t cp861 -f utf8 fdnpkg16.is8 > fdnpkg16.is
+iconv -t cp858 -f utf8 fdnpkg16.it8 > fdnpkg16.it
+iconv -t cp775 -f utf8 fdnpkg16.lt8 > fdnpkg16.lt
+iconv -t cp775 -f utf8 fdnpkg16.lv8 > fdnpkg16.lv
+iconv -t cp858 -f utf8 fdnpkg16.nl8 > fdnpkg16.nl
+iconv -t cp865 -f utf8 fdnpkg16.no8 > fdnpkg16.no
+iconv -t cp860 -f utf8 fdnpkg16.pb8 > fdnpkg16.pb
+iconv -t cp852 -f utf8 fdnpkg16.pl8 > fdnpkg16.pl
+iconv -t cp860 -f utf8 fdnpkg16.pt8 > fdnpkg16.pt
+iconv -t cp866 -f utf8 fdnpkg16.ru8 > fdnpkg16.ru
+iconv -t cp852 -f utf8 fdnpkg16.si8 > fdnpkg16.si
+iconv -t cp852 -f utf8 fdnpkg16.sk8 > fdnpkg16.sk
+iconv -t cp852 -f utf8 fdnpkg16.sl8 > fdnpkg16.sl
+iconv -t cp855 -f utf8 fdnpkg16.sr8 > fdnpkg16.sr
+iconv -t cp858 -f utf8 fdnpkg16.sv8 > fdnpkg16.sv
+iconv -t cp857 -f utf8 fdnpkg16.tr8 > fdnpkg16.tr
+iconv -t cp855 -f utf8 fdnpkg16.uk8 > fdnpkg16.uk
 
 rm fdnpkg16.*8
 cd ../..
